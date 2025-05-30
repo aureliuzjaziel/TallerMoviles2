967c1d4e3edc8fa7072da2ed72a85515c74994a9
aureliuzjaziel <xaviercitolagos@outlook.com>
2025-05-29 22:50:37 -0500

    navegacion entre screens

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -1,12 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function Navigator() {
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import PerfilScreen from '../screens/PerfilScreen'
+import JuegoScreen from '../screens/JuegoScreen'
+import ScoreScreen from '../screens/ScoreScreen'
+import WelcomeScreen from '../screens/WelcomeScreen'
+import loginScreen from '../screens/LoginScreen'
+import RegistroScreen from '../screens/RegistroScreen'
+
+// Importa tus screens aquí
+
+
+const Stack = createNativeStackNavigator()
+const Drawer = createDrawerNavigator()
+
+function MyDrawer() {
   return (
-    <View>
-      <Text>Navigator</Text>
-    </View>
+    <Drawer.Navigator>
+      <Drawer.Screen name="Perfil" component={PerfilScreen} />
+      <Drawer.Screen name="Juego" component={JuegoScreen} />
+      <Drawer.Screen name="Puntuacion" component={ScoreScreen} />
+    </Drawer.Navigator>
   )
 }
 
-const styles = StyleSheet.create({})
+function MyStack() {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Welcome">
+      <Stack.Screen name="Welcome" component={WelcomeScreen} />
+      <Stack.Screen name="Login" component={loginScreen} />
+      <Stack.Screen name="Registro" component={RegistroScreen} />
+      <Stack.Screen name="Drawer" component={MyDrawer} />
+    </Stack.Navigator>
+  )
+}
+
+export default function Navigator() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
+  )
+}
