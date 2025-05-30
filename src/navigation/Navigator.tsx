116f81aebe408a9ba282aee34bf17c76134c0082
fabricio302004 <leandro7moreira30@gmail.com>
2025-05-30 01:50:06 -0500

    final

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -2,9 +2,6 @@ import React from 'react'
 
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import { NavigationContainer } from '@react-navigation/native'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-
-
 import PerfilScreen from '../screens/PerfilScreen'
 import JuegoScreen from '../screens/JuegoScreen'
 import ScoreScreen from '../screens/ScoreScreen'
@@ -12,27 +9,18 @@ import WelcomeScreen from '../screens/WelcomeScreen'
 import loginScreen from '../screens/LoginScreen'
 import RegistroScreen from '../screens/RegistroScreen'
 import App from '../components/App'
+import { createStackNavigator } from '@react-navigation/stack'
 
 
  
 
 
-const Stack = createNativeStackNavigator()
+const Stack = createStackNavigator()
 const Drawer = createDrawerNavigator()
-
-function MyDrawer() {
-  return (
-    <Drawer.Navigator>
-      <Drawer.Screen name="Perfil" component={PerfilScreen} />
-      <Drawer.Screen name="Juego" component={JuegoScreen} />
-      <Drawer.Screen name="Puntuacion" component={ScoreScreen} />
-    </Drawer.Navigator>
-  )
-}
 
 function MyStack() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Welcome">
+    <Stack.Navigator screenOptions={{ headerShown: false }} >
       <Stack.Screen name="Welcome" component={WelcomeScreen} />
       <Stack.Screen name="Login" component={loginScreen} />
       <Stack.Screen name="Registro" component={RegistroScreen} />
@@ -41,6 +29,19 @@ function MyStack() {
     </Stack.Navigator>
   )
 }
+
+function MyDrawer() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Login" component={loginScreen} />
+      <Drawer.Screen name="Perfil" component={PerfilScreen} />
+      <Drawer.Screen name="Juego" component={JuegoScreen} />
+      <Drawer.Screen name="Puntuacion" component={ScoreScreen} />
+    </Drawer.Navigator>
+  )
+}
+
+
 
 export default function Navigator() {
   return (
