622f4cc5298044dbe95c7a8cfb3185ce06cc9318
aureliuzjaziel <xaviercitolagos@outlook.com>
2025-07-11 20:02:34 -0500

    commit de seguridad 2

--- supabase/config.tsx
+++ supabase/config.tsx
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-export const supabase = createClient('https://xglpaobtlskfotaekjqj.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhnbHBhb2J0bHNrZm90YWVranFqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwOTM2ODYsImV4cCI6MjA2MzY2OTY4Nn0.N9FU0SdBW1mR_XoZurmUQ7JflhFmE7CQfOYJ1k5Byu4')
+export const supabase = createClient('https://pvyauemetdjaibvrhtuh.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB2eWF1ZW1ldGRqYWlidnJodHVoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyNzkwNTAsImV4cCI6MjA2Nzg1NTA1MH0.lC19ooPP03iAZw6IclLGSiAWimhDuiEC853nixxAN5k')
 
